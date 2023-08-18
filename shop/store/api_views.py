817384fcd472836c74a9c7e49597886cb5ced7eb
cooper <tokyolabs@protonmail.com>
2023-08-18 12:57:16 -0400

    wip api

--- shop/store/api_views.py
+++ shop/store/api_views.py
@@ -1,4 +1,8 @@
 from rest_framework.generics import ListAPIView
+
+# from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
+
 from store.serializers import ProductSerializer
 from store.models import Product
 
@@ -6,3 +10,23 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # filter back ends with URL Query
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ("id",)
+
+    # filter a product by whether its on sale or not
+    def get_queryset(self):
+        on_sale = self.request.query_params.get("on_sale", None)
+        if on_sale is None:
+            return super().get_query()
+        queryset = Product.objects.all()
+        if on_sale.lower() == "true":
+            from django.utils import timezone
+
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now,
+            )
+        return queryset
