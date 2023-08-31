dfb2e85adc75494c4fde49abdcc22ab9a3e9c995
cooper <tokyolabs@protonmail.com>
2023-08-31 19:00:49 -0400

    fixed get_queryset error

--- shop/store/api_views.py
+++ shop/store/api_views.py
@@ -20,14 +20,14 @@ class ProductList(ListAPIView):
     search_fields = ("name", "description")
 
     # filter a product by whether its on sale or not
-    # def get_queryset(self):
-    #     on_sale = self.request.query_params.get("on_sale", None)
-    #     if on_sale is None:
-    #         return super().get_query()
-    #     queryset = Product.objects.all()
-    #     if on_sale.lower() == "true":
-    #         from django.utils import timezone
+    def get_queryset(self):
+        on_sale = self.request.query_params.get("on_sale", None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.lower() == "true":
+            from django.utils import timezone
 
-    #         now = timezone.now()
-    #         return queryset.filter(sale_start__lte=now, sale_end__gte=now)
-    #     return queryset
+            now = timezone.now()
+            return queryset.filter(sale_start__lte=now, sale_end__gte=now)
+        return queryset
