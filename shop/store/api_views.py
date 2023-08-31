bbb76f63427fcda9841a70a8190fc24fc71ed03e
cooper <tokyolabs@protonmail.com>
2023-08-31 18:55:32 -0400

    added search filter to api

--- shop/store/api_views.py
+++ shop/store/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView
-
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -14,19 +14,20 @@ class ProductList(ListAPIView):
     serializer_class = ProductSerializer
 
     # filter back ends with URL Query
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     # filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ("id",)
+    search_fields = ("name", "description")
 
     # filter a product by whether its on sale or not
-    def get_queryset(self):
-        on_sale = self.request.query_params.get("on_sale", None)
-        if on_sale is None:
-            return super().get_query()
-        queryset = Product.objects.all()
-        if on_sale.lower() == "true":
-            from django.utils import timezone
+    # def get_queryset(self):
+    #     on_sale = self.request.query_params.get("on_sale", None)
+    #     if on_sale is None:
+    #         return super().get_query()
+    #     queryset = Product.objects.all()
+    #     if on_sale.lower() == "true":
+    #         from django.utils import timezone
 
-            now = timezone.now()
-            return queryset.filter(sale_start__lte=now, sale_end__gte=now)
-        return queryset
+    #         now = timezone.now()
+    #         return queryset.filter(sale_start__lte=now, sale_end__gte=now)
+    #     return queryset
