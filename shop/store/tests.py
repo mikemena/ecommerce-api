a5ba9d2f56d7523dd0db736d319390d381781e3e
cooper <tokyolabs@protonmail.com>
2023-09-06 20:34:03 -0400

    wip

--- shop/store/tests.py
+++ shop/store/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from store.models import Product
 
-# Create your tests here.
+
+class ProductCreateTestCase(APITestCase):
+    def test_create_product(self):
+        initial_product_count = Product.objects.count()
+        product_attrs = {
+            "name": "New Product",
+            "description": "Some description.",
+            "price": "120.50",
+        }
+        response = self.client.post("/api/v1/products/new", product_attrs)
+        if response.status_code != 201:
+            print(response.data)
+        self.assertEqual(Product.objects.count(), initial_product_count + 1)
